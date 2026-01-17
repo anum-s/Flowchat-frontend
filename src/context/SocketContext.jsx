ac28e0512fa6de0443308c1df477173ae1bab15d
Anum <anums1113@gmail.com>
2026-01-17 19:34:56 +0500

    update

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -3,8 +3,6 @@ import {createContext,useContext, useEffect, useState} from 'react';
 import { useAuth } from './AuthContext';
 
 // const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
-const getBackendUrl = () =>
-  import.meta.env?.VITE_BACKEND_URL || "http://localhost:5000";
 
 const socketContext = createContext();
 
@@ -12,30 +10,42 @@ export const useSocketContext=()=>{
     return useContext(socketContext);
 }
 
+const getBackendUrl = () =>
+  import.meta.env?.VITE_BACKEND_URL || "http://localhost:5000";
+
 export const SocketContextProvider=({children})=>{
     const [socket,setSocket] = useState(null);
     const [onlineUsers,setOnlineUsers] = useState([]);
     const {authUser} = useAuth()
-    useEffect(()=>{
-        if(authUser){
-            const socket = io(getBackendUrl(),{
-                query:{
-                    userId:authUser?._id,
-                },
-                transports: ['websocket', 'polling'],
-        });
-        socket.on("getOnlineUsers",(users)=>{
-            setOnlineUsers(users)
-        });
-        setSocket(socket);
-        return()=>socket.close();
-        } else{
-            if(socket){
-             socket.close();
-             setSocket(null);   
-            }
-        }
-    },[authUser]);
+
+ useEffect(() => {
+    if (!authUser) {
+      if (socket) {
+        socket.close()
+        setSocket(null)
+      }
+      return
+    }
+
+    const newSocket = io(getBackendUrl(), {
+      query: {
+        userId: authUser?._id
+      },
+      transports: ['websocket', 'polling'],
+      withCredentials: true
+    })
+
+    newSocket.on('getOnlineUsers', users => {
+      setOnlineUsers(users)
+    })
+
+    setSocket(newSocket)
+
+    return () => {
+      newSocket.close()
+    }
+  }, [authUser])
+  
     return(
     <socketContext.Provider value={{socket,onlineUsers}}>
         {children}
