8c566f6a7b18dd38d485609a1e34d472da421684
Anum <anums1113@gmail.com>
2026-01-17 17:39:07 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const backendURL = process.env.VITE_BACKEND_URL || "http://localhost:5000"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server:{
     proxy:{
       '/api':{
-        target: import.meta.env.VITE_BACKEND_URL ,
+        target: backendURL ,
         changeOrigin: true,
         secure:false
       }
