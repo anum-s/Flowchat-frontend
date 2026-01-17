d1429396ad1f1fb199f54b70926d68a09d202768
Anum <anums1113@gmail.com>
2026-01-17 18:00:27 +0500

    Fix Vite config for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const backendURL = process.env.VITE_BACKEND_URL || "http://localhost:5000"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server:{
     proxy:{
       '/api':{
-        target: backendURL ,
+        target: "http://localhost:5000" ,
         changeOrigin: true,
         secure:false
       }
